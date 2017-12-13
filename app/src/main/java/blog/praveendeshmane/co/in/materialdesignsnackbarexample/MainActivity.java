d17691308ac82a7584f9203b2849a5983faca7f7
praveendeshmane <deshmane.praveen@gmail.com>
2017-12-14 00:06:35 +0530

    Initial commit

---
+++ app/src/main/java/blog/praveendeshmane/co/in/materialdesignsnackbarexample/MainActivity.java
@@ -0,0 +1,89 @@
+package blog.praveendeshmane.co.in.materialdesignsnackbarexample;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private CoordinatorLayout coordinatorLayout;
+    private Button btnSimpleSnackbar, btnActionCallback, btnCustomView;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        coordinatorLayout = findViewById(R.id
+                .coordinatorLayout);
+
+        fab = findViewById(R.id.fab);
+
+        mToolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+
+        btnSimpleSnackbar =  findViewById(R.id.btnSimpleSnackbar);
+        btnActionCallback =  findViewById(R.id.btnActionCallback);
+        btnCustomView =  findViewById(R.id.btnCustomSnackbar);
+
+        btnSimpleSnackbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Simple snackbar", Snackbar.LENGTH_LONG);
+
+                snackbar.show();
+            }
+        });
+
+        btnActionCallback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Snackbar with actionbar", Snackbar.LENGTH_LONG)
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Snackbar snackbar1 = Snackbar.make(coordinatorLayout, "Action call back method", Snackbar.LENGTH_SHORT);
+                                snackbar1.show();
+                            }
+                        });
+
+                snackbar.show();
+            }
+        });
+
+        btnCustomView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "custom snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("RETRY" , new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                            }
+                        });
+
+                // Changing action button text color
+                snackbar.setActionTextColor(Color.RED);
+
+                // Changing message text color
+                View sbView = snackbar.getView();
+                TextView textView = sbView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setTextColor(Color.YELLOW);
+
+                snackbar.show();
+            }
+        });
+    }
+}
